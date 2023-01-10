86f0d2f70324ac7590546c7079fbd9b52dcaa0a7
rameezraja <rameezraja2755@gmail.com>
2023-01-10 20:07:37 +0500

    more styles fixses

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,4 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import "../styles/animated.css";
-import '../node_modules/elegant-icons/style.css';
-import '../node_modules/et-line/style.css';
 import '../styles/style.scss';
 import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
 import Navbar from '../components/Navbar';
