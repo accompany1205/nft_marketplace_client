8cc793747c47e7b49745ac86099529fc23df87c4
ts-hiren <hiren@techstaunch.com>
2023-01-10 16:40:29 +0530

    Changed imports to scss for styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import "../styles/animated.css";
-import '../node_modules/font-awesome/css/font-awesome.min.css';
 import '../node_modules/elegant-icons/style.css';
 import '../node_modules/et-line/style.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../styles/style.scss';
 import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
 import Navbar from '../components/Navbar';
