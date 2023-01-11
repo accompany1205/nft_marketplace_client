ea0ef8832045c6e8b46f1b390f907de7a17c5131
ts-hiren <hiren@techstaunch.com>
2023-01-11 14:28:01 +0530

    feat(checkout): added place a bid modal

---
+++ components/checkouts/Checkout.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { CheckoutProps } from "./checkoutProps.type";
+
+const Checkout: React.FC<CheckoutProps> = ({
+  product,
+  onClose,
+  onCheckout,
+}) => {
+  const price = parseFloat(product.price);
+  const [quantity, setQuantity] = useState(1);
+  return (
+    <div className="checkout">
+      <div className="maincheckout">
+        <button className="btn-close" onClick={() => onClose()}>
+          x
+        </button>
+        <div className="heading">
+          <h3>Checkout</h3>
+        </div>
+        <p>
+          You are about to purchase a{" "}
+          <span className="bold">{product.productName}</span>
+          <span className="bold">from {product.owner.username}</span>
+        </p>
+        <div className="detailcheckout mt-4">
+          <div className="listcheckout">
+            <h6>
+              Enter quantity.
+              <span className="color">10 available</span>
+            </h6>
+            <input
+              type="number"
+              name="buy_now_qty"
+              id="buy_now_qty"
+              value={quantity}
+              onChange={(e) => setQuantity(parseInt(e.target.value))}
+              className="form-control"
+            />
+          </div>
+        </div>
+        <div className="heading mt-3">
+          <p>You will pay</p>
+          <div className="subtotal">{price * quantity}</div>
+        </div>
+        <button
+          className="btn-main lead mb-5"
+          onClick={() =>
+            onCheckout({
+              bid: price,
+              quantity,
+            })
+          }
+        >
+          Checkout
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Checkout;
--- pages/product/[pid].tsx
+++ pages/product/[pid].tsx
@@ -1,44 +1,79 @@
 import { useRouter } from "next/router";
+import { useState } from "react";
+import BidCheckout from "../../components/checkouts/BidCheckout";
+import Checkout from "../../components/checkouts/Checkout";
 import Loader from "../../components/Loader";
 import authApi from "../../redux/service/appService";
 
 const NftDetail = () => {
   const router = useRouter();
-  const { data: details, isLoading } = authApi.endpoints.getProductDetails.useQuery(router.query.pid && typeof router.query.pid === 'string' ? router.query.pid : '');
-  console.log(router.query.pid, 'pid')
+  const { data: details, isLoading } =
+    authApi.endpoints.getProductDetails.useQuery(
+      router.query.pid && typeof router.query.pid === "string"
+        ? router.query.pid
+        : ""
+    );
+  const [isBidCheckout, setIsBidCheckout] = useState(false);
+  const [isCheckout, setIsCheckout] = useState(false);
   const nft = {
     title: "Nike",
-    category: 'Sports Shoes',
+    category: "Sports Shoes",
     views: 190,
     likes: 200,
-    description: 'Best Shoe for sports',
+    description: "Best Shoe for sports",
     owner: {
-      username: 'Rameez Raja',
-    }
-  }
+      username: "Rameez Raja",
+    },
+  };
   return (
     <div className="greyscheme">
-      {isLoading ? <div className="d-flex  justify-content-center align-items-center" style={{ height: '100vh' }}><Loader /></div> :
-        <section className='container'>
-          <div className='row mt-md-5 pt-md-4'>
+      {isLoading ? (
+        <div
+          className="d-flex  justify-content-center align-items-center"
+          style={{ height: "100vh" }}
+        >
+          <Loader />
+        </div>
+      ) : (
+        <section className="container">
+          <div className="row mt-md-5 pt-md-4">
             <div className="col-md-6 text-center">
-              <img src='https://images.unsplash.com/photo-1552346154-21d32810aba3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80' className="img-fluid img-rounded mb-sm-30" alt="" />
+              <img
+                src="https://images.unsplash.com/photo-1552346154-21d32810aba3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+                className="img-fluid img-rounded mb-sm-30"
+                alt=""
+              />
             </div>
             <div className="col-md-6">
               <div className="item_info">
                 <h2>{details?.data?.specs?.productName}</h2>
                 <div className="item_info_counts">
-                  <div className="item_info_type"><i className="fa fa-image"></i>{details?.data.specs.brand}</div>
-                  <div className="item_info_views"><i className="fa fa-eye"></i>{details?.data.specs.views}</div>
-                  <div className="item_info_like"><i className="fa fa-heart"></i>{details?.data.specs.likes}</div>
+                  <div className="item_info_type">
+                    <i className="fa fa-image"></i>
+                    {details?.data.specs.brand}
+                  </div>
+                  <div className="item_info_views">
+                    <i className="fa fa-eye"></i>
+                    {details?.data.specs.views}
+                  </div>
+                  <div className="item_info_like">
+                    <i className="fa fa-heart"></i>
+                    {details?.data.specs.likes}
+                  </div>
                 </div>
                 <p>{details?.data?.specs?.description}</p>
                 <div className="spacer-40"></div>
                 <div className="de_tab">
                   <ul className="de_nav">
-                    <li id='Mainbtn0' className="active"><span >Details</span></li>
-                    <li id='Mainbtn' ><span >Bids</span></li>
-                    <li id='Mainbtn1' className=''><span >History</span></li>
+                    <li id="Mainbtn0" className="active">
+                      <span>Details</span>
+                    </li>
+                    <li id="Mainbtn">
+                      <span>Bids</span>
+                    </li>
+                    <li id="Mainbtn1" className="">
+                      <span>History</span>
+                    </li>
                   </ul>
                   <div className="de_tab_content">
                     <div className="tab-1 onStep fadeIn">
@@ -48,7 +83,11 @@ const NftDetail = () => {
                           <div className="item_author">
                             <div className="author_list_pp">
                               <span>
-                                <img className="lazy" src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80" alt="" />
+                                <img
+                                  className="lazy"
+                                  src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
+                                  alt=""
+                                />
                                 <i className="fa fa-check"></i>
                               </span>
                             </div>
@@ -67,29 +106,61 @@ const NftDetail = () => {
                         </div>
                       </div>
                     </div>
-                    <div className='detailcheckout mt-4'>
-                      <div className='listcheckout'>
-                        <h5>
-                          Price
-                        </h5>
-                        <div className='subtotal'>
+                    <div className="detailcheckout mt-4">
+                      <div className="listcheckout">
+                        <h5>Price</h5>
+                        <div className="subtotal">
                           {details?.data?.specs?.price}
                         </div>
                       </div>
                     </div>
                     <div className="d-flex flex-row mt-5">
-                      <button className='btn-main lead mb-5 me-3' >Buy Now</button>
-                      <button className='btn-main btn2 lead mb-5' >Place Bid</button>
+                      <button className="btn-main lead mb-5 me-3">
+                        Buy Now
+                      </button>
+                      <button
+                        className="btn-main btn2 lead mb-5"
+                        onClick={() => setIsBidCheckout(true)}
+                      >
+                        Place Bid
+                      </button>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
+          {/* details?.data.specs is kept in condition to make sure we have info */}
+          {isBidCheckout && !!details?.data.specs && (
+            <BidCheckout
+              onClose={() => setIsBidCheckout(false)}
+              product={{
+                owner: nft.owner,
+                ...details?.data.specs,
+              }}
+              onCheckout={(checkout) => {
+                console.log(checkout);
+                setIsBidCheckout(false);
+              }}
+            />
+          )}
+          {isCheckout && !!details?.data.specs && (
+            <Checkout
+              onClose={() => setIsCheckout(false)}
+              product={{
+                owner: nft.owner,
+                ...details?.data.specs,
+              }}
+              onCheckout={(checkout) => {
+                console.log(checkout);
+                setIsCheckout(false);
+              }}
+            />
+          )}
         </section>
-      }
+      )}
     </div>
   );
-}
+};
 
 export default NftDetail;
