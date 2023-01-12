8ba5f8a3e1d6323f56f7ecc82d29f0abe72c964d
Hiren F <80745166+ts-hiren@users.noreply.github.com>
2023-01-12 19:50:24 +0530

    corrected variable

--- pages/product/[product].tsx
+++ pages/product/[product].tsx
@@ -121,7 +121,7 @@ const NftDetail = () => {
                         {details?.data?.variants && (
                           <div className="row mt-5">
                             {details?.data?.variants.map((variant, index) => (
-                              <div className="col-lg-4 col-md-6 col-sm-6" key={key}>
+                              <div className="col-lg-4 col-md-6 col-sm-6" key={index}>
                                 <div className="nft_attr">
                                   <h4>{variant.size}</h4>
                                   <span>BID</span>
