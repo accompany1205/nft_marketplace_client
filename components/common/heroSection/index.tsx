bd95b6b03d207f344b87bebdcc2ddc811f8ff50c
keval navadiya <kevalnavadiya39@gmail.com>
2023-02-21 11:44:09 +0530

    WIP

---
+++ components/common/heroSection/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+interface IProps {
+    bgImgUrl:string
+    title: string
+    children?: JSX.Element[] | JSX.Element
+}
+
+const HeroSection:React.FC<IProps> = ({ bgImgUrl, title, children }) => (
+  <section className="text-light" data-bgimage={`url(${bgImgUrl})`} data-stellar-background-ratio=".2">
+    <div className="center-y relative text-center" data-scroll-speed={4}>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 offset-md-2">
+            <div className="col text-center">
+              <div className="spacer-single" />
+              <h1>{title}</h1>
+              {children || <div className="spacer-20" />}
+            </div>
+            <div className="clearfix" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+);
+
+HeroSection.defaultProps = {
+  children: undefined,
+};
+
+export default HeroSection;
