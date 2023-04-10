63cb8056361502c137b509754100444a277fcf4f
Sidd <s.notani@outlook.com>
2023-04-10 12:26:06 +0100

    Add onclick props to nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -24,6 +24,7 @@ import WalletConnector from './WalletConnector';
 interface INavProps {
   children: JSX.Element | string;
   href: string;
+  onClick: () => void;
 }
 
 setDefaultBreakpoints([{ xs: 0 }, { l: 1199 }, { xl: 1200 }]);
